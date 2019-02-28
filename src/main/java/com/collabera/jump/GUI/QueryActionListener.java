e9e64b5ed2f375260382379a11ae8e188411c846
JoeTobon <jet33@njit.edu>
2019-02-28 10:35:32 -0500

    Query implenmentation

--- src/main/java/com/collabera/jump/GUI/QueryActionListener.java
+++ src/main/java/com/collabera/jump/GUI/QueryActionListener.java
@@ -2,6 +2,17 @@ package com.collabera.jump.GUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Properties;
 
 import javax.swing.JTextField;
 
@@ -14,18 +25,76 @@ public class QueryActionListener implements ActionListener
 		this.textField = textField;
 	}
 
-	public void actionPerformed(ActionEvent e) 
+	public void actionPerformed(ActionEvent a) 
 	{
-		switch(e.getActionCommand())
+		switch(a.getActionCommand())
 		{
 			case "DB":
 				System.out.println("DB");
 				break;
-			case "Execute":
-				System.out.println("Execute");
+			case "Execute":	
+				try 
+				{
+					Properties properties = new Properties();
+	
+					properties.load(new FileInputStream("config.properties"));
+	
+					Connection connection = DriverManager.getConnection(properties.getProperty("url"),
+							properties.getProperty("name"), properties.getProperty("password"));
+					
+					if(!connection.isValid(1))
+					{
+						System.out.println("Please re-enter credentials!");						
+					}
+					else
+					{
+						Statement statement = connection.createStatement();
+						
+						String query = textField.getText();
+						System.out.println(query);
+			
+						try 
+						{
+			
+							ResultSet resultSet = statement.executeQuery(query);		
+			
+							while (resultSet.next()) 
+							{
+								ResultSetMetaData metadata = resultSet.getMetaData();
+			
+								String data[] = new String[metadata.getColumnCount()];
+			
+								for (int i = 1; i <= metadata.getColumnCount(); i++) 
+								{
+									data[i - 1] = resultSet.getString(i);
+								}
+			
+								System.out.println(Arrays.toString(data));
+			
+							}
+						}
+						catch (SQLException e) 
+						{
+							System.out.println("Invalid SQL Query!");
+						}
+					}
+				} 
+				catch (SQLException e) 
+				{
+					System.out.println("Invalid DB credentials. Re-enter!");
+				} 
+				catch (FileNotFoundException e) 
+				{
+					System.out.println("File not found!");
+				} 
+				catch (IOException e) 
+				{
+					System.out.println("IO exception");
+				}
+				
 				break;
 			case "Clear":
-				System.out.println("Clear");
+				textField.equals("");
 				break;
 		}
 		
